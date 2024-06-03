b3cac8825282fc27682d622a06b1e863807873e6
jessclark1234 <jessicaclark7890@gmail.com>
2024-06-03 14:59:11 -0700

    linked css and js to html

---
+++ index.js
@@ -0,0 +1,9 @@
+function oneButton() {
+  var button = document.querySelector("#button1");
+  alert("Gotteeee");
+    console.log('goteeeee')
+  
+}
+button.addEventListener("click", oneButton);
+
+oneButton();
